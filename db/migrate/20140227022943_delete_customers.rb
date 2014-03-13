ce799a9dc22d3d4f4a3bb0f3d4af11e16d6f31fb
Paul <meggsp@gmail.com>
2014-03-13 16:40:15 +0000

    Heroku

--- db/migrate/20140227022943_delete_customers.rb
+++
@@ -1,5 +0,0 @@
-class DeleteCustomers < ActiveRecord::Migration
-  def change
-    drop_table :customers
-  end
-end
--- db/migrate/20140305010649_add_column_customer_id_to_quotes.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnCustomerIdToQuotes < ActiveRecord::Migration
-  def change
-    add_column :quotes, :customer_id, :integer
-  end
-end
